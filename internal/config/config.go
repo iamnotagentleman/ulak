ce408a5c042923151a95d4409dee16244d04db5d
Veli Eroglu <realveli24@gmail.com>
2025-10-25 03:19:14 +0300

    fix: typo in config

--- internal/config/config.go
+++ internal/config/config.go
@@ -48,7 +48,7 @@ type Auth struct {
 }
 
 type MessageWorker struct {
-	MaxRecordsPerRead         int           `env:"MAX_RECORDS_PER_RECORD" default:"10"`
+	MaxRecordsPerRead         int           `env:"MAX_RECORDS_PER_READ" default:"10"`
 	MessageChannelSendTimeout int           `env:"MESSAGE_CHANNEL_SEND_TIMEOUT" default:"10"`
 	RateLimitPerMinute        int           `env:"RATE_LIMIT_PER_MINUTE" default:"1"`
 	RateLimitBurst            int           `env:"RATE_LIMIT_BURST" default:"2"`
